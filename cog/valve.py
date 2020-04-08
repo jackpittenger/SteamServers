4c1db1a4d1f2cf10f899f84dc8ccdac865cfba0a
realSaddy <jrp30074@gmail.com>
2020-04-07 19:45:04 -0500

    feat(players): pquery lookup command

--- cog/valve.py
+++ cog/valve.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from helpers import query_logic
+from helpers import query_logic, players_logic
 
 
 class Valve(commands.Cog):
@@ -14,12 +14,23 @@ class Valve(commands.Cog):
             """
             return await query_logic(ctx, address)
 
+        @bot.command()
+        async def pquery(ctx, address):
+            """
+            Lists players on a server
+            :param ctx:
+            :param address:
+            :return:
+            """
+            return await players_logic(ctx, address)
+
         @query.error
+        @pquery.error
         async def do_repeat_handler(ctx, error):
-            print(error)
             if isinstance(error, commands.MissingRequiredArgument):
                 if error.param.name == 'address':
-                    await ctx.send("Please format your command like: `s!query 144.12.123.51:27017`")
+                    print(ctx)
+                    await ctx.send("Please format your command like: `s!command 144.12.123.51:27017`")
 
 
 def setup(bot):
