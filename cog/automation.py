95eca5b1d5fcc7302ad6dfc5e18c835cba90db87
realSaddy <jrp30074@gmail.com>
2020-04-11 21:48:16 -0500

    fix(automation): autostatus working on main event loop

--- cog/automation.py
+++ cog/automation.py
@@ -4,8 +4,8 @@ from cog.database import get_server
 from helpers import query_logic
 from discord.ext import commands
 
-
 timers = {}
+
 
 class Automation(commands.Cog):
     def __init__(self, bot):
@@ -15,28 +15,32 @@ class Automation(commands.Cog):
             """
             Queries a saved server automatically in determined interval
             s!autostatus name minutes
-            s!status ToasterRP 1440
+            s!autostatus myServer 45
             """
-            if minutes <= 0:
-                return await ctx.send('Minutes should be greater than \'0\'!')
-            
-            await create_status_timer(bot, ctx, name, minutes)
+            if 1440 < minutes < 30:
+                return await ctx.send('Minutes should be greater than 30 and less than 1440 minutes!')
 
-async def create_status_timer(bot, ctx, name, minutes):
+            await create_status_timer(bot, ctx, name, minutes * 60)
+
+
+async def create_status_timer(bot, ctx, name, seconds):
     server = await get_server(bot, ctx, name)
     if server:
         bot.db.servers.update_one(
-            { 'discord_server': ctx.guild.id, 'name': name }, 
-            { "$set": { 'timer': { 'channel': ctx.channel.id, 'interval': minutes } } }
+            {'discord_server': ctx.guild.id, 'name': name},
+            {"$set": {'timer': {'channel': ctx.channel.id, 'interval': seconds}}}
         )
-        _start_timer(bot, ctx.channel.id, server["address"], minutes)
+        _start_timer(bot, ctx.channel.id, server["address"], seconds)
+
 
 def _initialize_timers(bot):
-    results = bot.db.servers.find({ 'timer':  { '$exists': True } })
+    results = bot.db.servers.find({'timer': {'$exists': True}})
     for server in results:
+        print(server['timer']['channel'])
         _start_timer(bot, server["timer"]["channel"], server["address"], server["timer"]["interval"])
 
-def _start_timer(bot, channel_id, serverAddress, minutes):
+
+def _start_timer(bot, channel_id, server_address, seconds):
     async def sender(*args, **kwargs):
         channel = bot.get_channel(channel_id)
 
@@ -45,18 +49,14 @@ def _start_timer(bot, channel_id, serverAddress, minutes):
 
     def wrapper():
         try:
-            asyncio.run(query_logic(None, serverAddress, sender))
+            asyncio.run_coroutine_threadsafe(query_logic(None, server_address, sender), bot.loop)
         finally:
-            _start_timer(bot, channel_id, serverAddress, minutes)
-    
-    # create a new timer
-    timer = Timer(minutes, wrapper)
-    
-    # store the current timer
-    timers[str(channel_id) + ":" + serverAddress] = timer
-    
-    # start timer thread
+            _start_timer(bot, channel_id, server_address, seconds)
+
+    timer = Timer(seconds, wrapper)
+    timers[str(channel_id) + ":" + server_address] = timer
     timer.start()
+
 
 def setup(bot):
     bot.add_cog(Automation(bot))
