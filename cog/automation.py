7892d0e72f7bed80fd62640d5c4ac18fa8646407
realSaddy <jrp30074@gmail.com>
2020-07-29 22:39:20 -0500

    feat(rewrite): rewrite of automation to reduce RAM

--- cog/automation.py
+++ cog/automation.py
@@ -1,10 +1,14 @@
 import asyncio
-from threading import Timer
+import time
+import datetime
+import math
+from threading import Thread, Timer
 from cog.database import get_server
 from helpers import query_logic
 from discord.ext import commands
 
 timers = {}
+start_minutes = math.floor((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() / 60) 
 
 
 class Automation(commands.Cog):
@@ -20,7 +24,10 @@ class Automation(commands.Cog):
             """
             if minutes < 30 or minutes > 1440:
                 return await ctx.send('Minutes should be greater than 30 and less than 1440!')
-            await create_status_timer(bot, ctx, name, minutes * 60)
+            bot.db.servers.update_one(
+                {'discord_server': ctx.guild.id, 'name': name},
+                {"$set": {'timer': {'channel': ctx.channel.id, 'interval': minutes * 60}}}
+            )
             await ctx.send('Auto status successfully added to server \'%s\'' % name)
 
         @bot.command()
@@ -31,55 +38,42 @@ class Automation(commands.Cog):
             s!removeauto name
             s!removeauto myServer
             """
-            if str(ctx.guild.id)+name not in timers:
+            if bot.db.servers.find_one({'discord_server': ctx.guild.id, 'name': name, 'timer': {'$exists': True}}) is None:
                 return await ctx.send("That server is not currently setup!")
             bot.db.servers.update_one(
                 {'discord_server': ctx.guild.id, 'name': name},
                 {"$unset": {'timer': None}}
             )
-            timers[str(ctx.guild.id) + name].cancel()
-            timers[str(ctx.guild.id) + name] = None
             return await ctx.send("Server removed from autostatus!")
 
 
-async def create_status_timer(bot, ctx, name, seconds):
-    server = await get_server(bot, ctx, name)
-    if server:
-        bot.db.servers.update_one(
-            {'discord_server': ctx.guild.id, 'name': name},
-            {"$set": {'timer': {'channel': ctx.channel.id, 'interval': seconds}}}
-        )
-        _start_timer(bot, ctx.channel.id, server["address"], seconds, name, ctx.guild.id)
-
-
-def _initialize_timers(bot):
-    results = bot.db.servers.find({'timer': {'$exists': True}})
-    for server in results:
-        _start_timer(bot, server["timer"]["channel"], server["address"], server["timer"]["interval"], server['name'],
-                     server["discord_server"])
-
-
-def _start_timer(bot, channel_id, server_address, seconds, name, d_id):
-    async def sender(*args, **kwargs):
-        channel = bot.get_channel(channel_id)
-
-        if channel:
-            await channel.send(*args, **kwargs)
-
-    def wrapper():
-        try:
-            asyncio.run_coroutine_threadsafe(query_logic(None, server_address, sender, name, bot, d_id), bot.loop)
-        finally:
-            _start_timer(bot, channel_id, server_address, seconds, name, d_id)
-
-    # Run on bot startup
-    asyncio.run_coroutine_threadsafe(query_logic(None, server_address, sender, name, bot, d_id), bot.loop)
-
-    timer = Timer(seconds, wrapper)
-    timers[str(d_id)+name] = timer
-    timer.start()
+class Threaded(Thread):
+    def __init__(self, bot):
+        self.bot = bot
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    
+    def run(self):
+        time.sleep(10) # Temporary way to let the bot boot up - should probably come up with a better way
+        while True:
+            asyncio.run_coroutine_threadsafe(self.server_fetch(self.bot), self.bot.loop)
+            time.sleep(60)
+ 
+    async def server_fetch(self, bot):
+        results = bot.db.servers.find({'timer': {'$exists': True}})
+        time_now = math.floor((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() / 60)
+        for server in results:
+            if start_minutes-time_now != 0 and (time_now - start_minutes) % (server["timer"]["interval"] / 60) != 0:
+                continue
+            async def sender(*args, **kwargs):
+                channel = self.bot.get_channel(server["timer"]["channel"])
+                if channel:
+                    await channel.send(*args, **kwargs)
+        
+            await query_logic(None, server["address"], sender, server["name"], bot, server["discord_server"]) 
 
 
 def setup(bot):
     bot.add_cog(Automation(bot))
-    _initialize_timers(bot)
+    Threaded(bot)
